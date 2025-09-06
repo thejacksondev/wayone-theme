bb8a5cdaab77601513b9b401c511789b72dc2015
Nicholas Jackson <thejacksondev@gmail.com>
2025-09-06 10:53:49 -0700

    leaving nav alone for now

--- app/public/wp-content/themes/wayone/template-parts/hero.php
+++ app/public/wp-content/themes/wayone/template-parts/hero.php
@@ -1,6 +1,26 @@
-<section class="section hero">
+<section class="hero">
   <div class="container">
-  <h1>Wayone — coming soon</h1>
-  <p>Marketing page scaffold.</p>
-</div>
+    <div class="row">
+      <div class="col-xs-12 col-md-6">
+        <div class="text-box">
+          <h1>
+            <span>One Design.</span>
+            <span>Every Ad Format.</span>
+            <span>Done.</span>
+          </h1>
+          <p>Upload one design, and instantly create all your ads for Google, Meta, LinkedIn, and beyond.</p>
+          <div class="actions">
+            <a href="#" class="button">
+              <span>Create Now</span>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div class="col-xs-12 col-md-6">
+        <div class="image-box">
+          <img src="https://placehold.co/319x477" alt="Placeholder image" class="img-responsive">
+        </div>
+      </div>
+    </div>
+  </div>
 </section>
