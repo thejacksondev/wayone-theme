cf7d7604b353750d2990264e730fe6508287435c
Nicholas Jackson <thejacksondev@gmail.com>
2025-09-14 02:36:19 -0700

    footer complete

--- app/public/wp-content/themes/wayone/footer.php
+++ app/public/wp-content/themes/wayone/footer.php
@@ -1,6 +1,116 @@
-<footer class="site-footer" role="contentinfo">
+<footer class="footer" role="contentinfo">
   <div class="container">
-    <p>&copy; <?php echo date('Y'); ?> wayone</p>
+    <div class="row">
+      <div class="col-xs-12 col-sm-6 col-md-3">
+        <h3>Product</h3>
+        <ul>
+          <li><a href="#">Wayone Overview</a></li>
+          <li><a href="#">Wayone Code</a></li>
+          <li><a href="#">Wayone Pricing Plans</a></li>
+          <li><a href="#">Wayone.ai login</a></li>
+      </div>
+      <div class="col-xs-12 col-sm-6 col-md-3">
+        <h3>API Platform</h3>
+        <ul>
+          <li><a href="#">API Overview</a></li>
+          <li><a href="#">Developer Docs</a></li>
+          <li><a href="#">Pricing</a></li>
+          <li><a href="#">Console Login</a></li>
+      </div>
+      <div class="col-xs-12 col-sm-6 col-md-3">
+        <h3>Get Support</h3>
+        <ul>
+          <li><a href="#">Contact Support</a></li>
+          <li><a href="#">FAQs &amp; Help Center</a></li>
+          <li><a href="#">Customer Success</a></li>
+          <li><a href="#">Hire a professional partner</a></li>
+        </ul>
+      </div>
+      <div class="col-xs-12 col-sm-6 col-md-3">
+        <h3>Terms and Policies</h3>
+        <ul>
+          <li><a href="#">Privacy choices</a></li>
+          <li><a href="#">Privacy policy</a></li>
+          <li><a href="#">Terms of service - consumer</a></li>
+          <li><a href="#">Terms of service - commercial</a></li>
+          <li><a href="#">Usage policy</a></li>
+        </ul>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-xs-12 col-sm-12">
+        <p class="footer-logo"><strong><a href="<?php echo home_url(); ?>">Wayone</a></strong></p>
+      </div>
+      <div class="col-xs-12 col-sm-12">
+        <ul class="footer-social-links">
+          <li>
+            <a href="#">
+              <svg style="max-width: 24px;" height="200px" width="200px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 382 382" xml:space="preserve" fill="#000000" stroke="#000000">
+                <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                <g id="SVGRepo_iconCarrier">
+                  <path style="fill:#ffffff;" d="M347.445,0H34.555C15.471,0,0,15.471,0,34.555v312.889C0,366.529,15.471,382,34.555,382h312.889 C366.529,382,382,366.529,382,347.444V34.555C382,15.471,366.529,0,347.445,0z M118.207,329.844c0,5.554-4.502,10.056-10.056,10.056 H65.345c-5.554,0-10.056-4.502-10.056-10.056V150.403c0-5.554,4.502-10.056,10.056-10.056h42.806 c5.554,0,10.056,4.502,10.056,10.056V329.844z M86.748,123.432c-22.459,0-40.666-18.207-40.666-40.666S64.289,42.1,86.748,42.1 s40.666,18.207,40.666,40.666S109.208,123.432,86.748,123.432z M341.91,330.654c0,5.106-4.14,9.246-9.246,9.246H286.73 c-5.106,0-9.246-4.14-9.246-9.246v-84.168c0-12.556,3.683-55.021-32.813-55.021c-28.309,0-34.051,29.066-35.204,42.11v97.079 c0,5.106-4.139,9.246-9.246,9.246h-44.426c-5.106,0-9.246-4.14-9.246-9.246V149.593c0-5.106,4.14-9.246,9.246-9.246h44.426 c5.106,0,9.246,4.14,9.246,9.246v15.655c10.497-15.753,26.097-27.912,59.312-27.912c73.552,0,73.131,68.716,73.131,106.472 L341.91,330.654L341.91,330.654z"></path>
+                </g>
+              </svg>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <svg style="max-width: 22px;" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#000000">
+                <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                <g id="SVGRepo_iconCarrier">
+                  <path d="M9 22H15C20 22 22 20 22 15V9C22 4 20 2 15 2H9C4 2 2 4 2 9V15C2 20 4 22 9 22Z" stroke="#ffffff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+                  <path d="M12 15.5C13.933 15.5 15.5 13.933 15.5 12C15.5 10.067 13.933 8.5 12 8.5C10.067 8.5 8.5 10.067 8.5 12C8.5 13.933 10.067 15.5 12 15.5Z" stroke="#ffffff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+                  <path d="M17.6361 7H17.6477" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+                </g>
+              </svg>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <svg style="max-width: 19px;" fill="#ffffff" viewBox="0 0 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg" stroke="#ffffff">
+                <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                <g id="SVGRepo_iconCarrier">
+                  <title>tiktok</title>
+                  <path d="M16.656 1.029c1.637-0.025 3.262-0.012 4.886-0.025 0.054 2.031 0.878 3.859 2.189 5.213l-0.002-0.002c1.411 1.271 3.247 2.095 5.271 2.235l0.028 0.002v5.036c-1.912-0.048-3.71-0.489-5.331-1.247l0.082 0.034c-0.784-0.377-1.447-0.764-2.077-1.196l0.052 0.034c-0.012 3.649 0.012 7.298-0.025 10.934-0.103 1.853-0.719 3.543-1.707 4.954l0.020-0.031c-1.652 2.366-4.328 3.919-7.371 4.011l-0.014 0c-0.123 0.006-0.268 0.009-0.414 0.009-1.73 0-3.347-0.482-4.725-1.319l0.040 0.023c-2.508-1.509-4.238-4.091-4.558-7.094l-0.004-0.041c-0.025-0.625-0.037-1.25-0.012-1.862 0.49-4.779 4.494-8.476 9.361-8.476 0.547 0 1.083 0.047 1.604 0.136l-0.056-0.008c0.025 1.849-0.050 3.699-0.050 5.548-0.423-0.153-0.911-0.242-1.42-0.242-1.868 0-3.457 1.194-4.045 2.861l-0.009 0.030c-0.133 0.427-0.21 0.918-0.21 1.426 0 0.206 0.013 0.41 0.037 0.61l-0.002-0.024c0.332 2.046 2.086 3.59 4.201 3.59 0.061 0 0.121-0.001 0.181-0.004l-0.009 0c1.463-0.044 2.733-0.831 3.451-1.994l0.010-0.018c0.267-0.372 0.45-0.822 0.511-1.311l0.001-0.014c0.125-2.237 0.075-4.461 0.087-6.698 0.012-5.036-0.012-10.060 0.025-15.083z"></path>
+                </g>
+              </svg>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <svg style="max-width: 25px;" viewBox="0 -3 20 20" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" fill="#000000" stroke="#000000">
+                <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                <g id="SVGRepo_iconCarrier">
+                  <title>youtube [#168]</title>
+                  <desc>Created with Sketch.</desc>
+                  <defs> </defs>
+                  <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                    <g id="Dribbble-Light-Preview" transform="translate(-300.000000, -7442.000000)" fill="#ffffff">
+                      <g id="icons" transform="translate(56.000000, 160.000000)">
+                        <path d="M251.988432,7291.58588 L251.988432,7285.97425 C253.980638,7286.91168 255.523602,7287.8172 257.348463,7288.79353 C255.843351,7289.62824 253.980638,7290.56468 251.988432,7291.58588 M263.090998,7283.18289 C262.747343,7282.73013 262.161634,7282.37809 261.538073,7282.26141 C259.705243,7281.91336 248.270974,7281.91237 246.439141,7282.26141 C245.939097,7282.35515 245.493839,7282.58153 245.111335,7282.93357 C243.49964,7284.42947 244.004664,7292.45151 244.393145,7293.75096 C244.556505,7294.31342 244.767679,7294.71931 245.033639,7294.98558 C245.376298,7295.33761 245.845463,7295.57995 246.384355,7295.68865 C247.893451,7296.0008 255.668037,7296.17532 261.506198,7295.73552 C262.044094,7295.64178 262.520231,7295.39147 262.895762,7295.02447 C264.385932,7293.53455 264.28433,7285.06174 263.090998,7283.18289" id="youtube-[#168]"> </path>
+                      </g>
+                    </g>
+                  </g>
+                </g>
+              </svg>
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div class="col-xs-12 col-sm-12">
+        <p class="footer-text">
+          <span class="copyright">© <?php echo date('Y'); ?> Wayone.</span>
+          <span class="separator">|</span>
+          <span class="recaptcha">This Site Is Protected By Recaptcha Enterprise.</span>
+          <span class="separator">|</span>
+          <span class="privacy">The Google <a href="#">Privacy Policy</a> And <a href="#">Terms Of Service</a> Apply.</span>
+        </p>
+      </div>
+    </div>
   </div>
 </footer>
 
